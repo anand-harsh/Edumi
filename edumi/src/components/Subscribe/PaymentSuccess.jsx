c3907ebe8eef41c86c3a19f9f7bad44c51c4130e
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2023-06-17 23:23:04 +0530

    Update #7

--- edumi/src/components/Subscribe/PaymentSuccess.jsx
+++ edumi/src/components/Subscribe/PaymentSuccess.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Container, Heading, VStack, Box, Text , Button} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { RiCheckboxCircleFill } from 'react-icons/ri';
+import React from 'react';
 
 const PaymentSuccess = () => {
   return (
-    <div>PaymentSuccess</div>
-  )
-}
+    <Container>
+      <Heading children="You have a pro pack" my="8" textAlign={'center'} />
+      <VStack
+        boxShadow={'lg'}
+        pb="16"
+        alignItems={'center'}
+        borderRadius={'lg'}
+      >
+        <Box w="full" bg="yellow.400" css={{ borderRadius: '8px 8px 0 0' }}>
+          <Text>Payment Success</Text>
+        </Box>
 
-export default PaymentSuccess
+        <Box p="4">
+          <VStack textAlign={'center'} px="8" mt="4" spacing="8">
+            <Text>Congratulations You are a Pro member !!</Text>
+
+            <Heading size={'4xl'}>
+              <RiCheckboxCircleFill />
+            </Heading>
+          </VStack>
+        </Box>
+
+        <Link to="/">
+          <Button variant='ghost'>Go to Profile</Button>
+        </Link>
+
+        <Heading size='xs'>
+          Reference: lbclsebcobsechse8csedchse90
+        </Heading>
+      </VStack>
+    </Container>
+  );
+};
+
+export default PaymentSuccess;

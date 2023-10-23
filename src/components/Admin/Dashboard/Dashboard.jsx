d8e97b17330a1630f393d2834a8fd0b96b057eac
rajvinder-kaur <kaurrajvinder736@gmail.com>
2023-10-23 22:04:28 +0530

    commit dashboard layout

--- src/components/Admin/Dashboard/Dashboard.jsx
+++ src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box, Text, Heading, Stack, HStack, Progress } from '@chakra-ui/react';
+import { Grid, Box, Text, Heading, Stack, HStack, Progress, GridItem,Center } from '@chakra-ui/react';
 import Sidebar from '../Sidebar';
 import { RiArrowUpDownFill, RiArrowUpLine } from 'react-icons/ri';
 import { DoughnutChart, LineChart } from './Chart';
@@ -9,7 +9,7 @@ const Databox = ({ title, qty, qtyPercentage, profit }) => (
   <Box
     w={['full', '20%']}
     h={'20%'}
-    boxShadow={'-2px 0 10px rgba(107, 70, 193, 0.5)'}
+    boxShadow={'10px 10px 20px #d9d9d9'}
     p="8"
     borderRadius={'lg'}
   >
@@ -29,13 +29,13 @@ const Databox = ({ title, qty, qtyPercentage, profit }) => (
     <Text opacity={0.6} children={'Since last month'} />
   </Box>
 );
-const Bar=({title, value, profit})=>(
+const Bar = ({ title, value, profit }) => (
   <Box py="4" px={['0', '20']}>
-    <Heading size="sm" children={title} mb="2"/>
+    <Heading size="sm" children={title} mb="2" />
     <HStack w="full" alignItems={'center'}>
-      <Text children={profit?"0%":`-${value}%`}/> {/* fix */}
-      <Progress w="full" value={profit?value:0} colorScheme="blue"/>
-      <Text children={`${value>100?value:100}%`}/>
+      <Text children={profit ? "0%" : `-${value}%`} /> {/* fix */}
+      <Progress w="full" value={profit ? value : 0} colorScheme="blue" />
+      <Text children={`${value > 100 ? value : 100}%`} />
     </HStack>
   </Box>
 )
@@ -43,79 +43,95 @@ const Dashboard = () => {
   return (
     <Grid
       css={{
-        cusrsor: `url(), default`, // need to be updated
+        cursor: `url(), default`, // need to be updated
       }}
       minH={'100vh'}
-      templateColumns={['1fr', '5fr 1fr']}
+      templateColumns='repeat(3, 1fr)'
     >
-      <Box boxSizing="border-box" py="16" px={['4', '0']}>
+      {/* this shows the updation status of Dashboard */}
+      <GridItem rowStart={2} colStart={1} colEnd={4}  >
         <Text
           textAlign={'center'}
           opacity={0.5}
           children={`Last change was on ${String(new Date()).split('G')[0]}`}
         />
-      </Box>
+      </GridItem>
 
-      <Heading
+      {/* heading thats says dashboard  */}
+      <GridItem rowStart={1} colStart={1} colEnd={4} > <Heading
         children="Dashboard"
-        ml={['0', '16']}
+        
         mb="16"
-        textAlign={['center', 'left']}
-      />
-      <Stack
-        direction={['column', 'row']}
-        minH={'24'}
-        justifyContent={'space-evenly'}
-      >
-        <Databox title="Views" qty={123} qtyPercentage={30} profit={true} />
-        <Databox title="Users" qty={23} qtyPercentage={78} profit={true} />
-        <Databox
-          title="Subscription"
-          qty={12}
-          qtyPercentage={67}
-          profit={true}
-        />
-      </Stack>
+        textAlign={'center'}
+      /></GridItem>
 
-      <Sidebar />
-      <Box
-        m={['0', '16']}
-        borderRadius={'lg'}
-        p={['0', '16']}
-        mt={['4', '-66']}
-        boxShadow={'-2px 0 10px rgba(107, 70, 193, 0.5)'}
-      >
-        <Heading
-          textAlign={['center', 'left']}
-          size="md"
-          children="Views Graph"
-          pt={['8', '0']}
-          ml={['0', '10']}
-        />
-        {/* Line graph here */}
-        <LineChart />
-        <Grid templateColumns={['1fr', '2fr 1fr']} />
-        <Box p="4">
+      {/* this item is the sidebar of the contents */}
+      <GridItem colStart={1} colEnd={4} rowStart={3} >
+        <Center>
+        <Sidebar />
+        </Center>
+      </GridItem>
+      {/* this  a group of tabs */}
+      <GridItem colStart={1} colEnd={4} rowStart={4} >
+        <Stack
+          direction={['column', 'row']}
+          minH={'24'}
+          justifyContent={'space-evenly'}
+          p="2%"
+        >
+          <Databox title="Views" qty={123} qtyPercentage={30} profit={true} />
+          <Databox title="Users" qty={23} qtyPercentage={78} profit={true} />
+          <Databox
+            title="Subscription"
+            qty={12}
+            qtyPercentage={67}
+            profit={true}
+          />
+        </Stack>
+      </GridItem>
+      {/* this grid item shows the box of the line graph */}
+      <GridItem rowstart={5} colStart={1} colEnd={4} pt="5%" >
+        <Box
+          m={['0', '16']}
+          borderRadius={'lg'}
+          p={['0', '16']}
+          mt={['4', '-66']}
+          boxShadow={'10px 10px 20px #d9d9d9'}
+        >
           <Heading
             textAlign={['center', 'left']}
             size="md"
-            children="Progress Bar"
-            my="8"
-            ml={['0', '16']}
+            children="Views Graph"
+            pt={['8', '0']}
+            ml={['0', '10']}
           />
+          {/* Line graph here */}
+          <LineChart />
+          <Grid templateColumns={['1fr', '2fr 1fr']} />
+          <Box p="4">
+            <Heading
+              textAlign={['center', 'left']}
+              size="md"
+              children="Progress Bar"
+              my="8"
+              ml={['0', '16']}
+            />
+          </Box>
+          <Bar profit={true} title="Views" value={30} />
+          <Bar profit={true} title="Users" value={78} />
+          <Bar profit={true} title="Subscriptions" value={0} />
         </Box>
-        <Bar profit={true} title="Views" value={30}/>
-        <Bar profit={true} title="Users" value={78}/>
-        <Bar profit={true} title="Subscriptions" value={0}/>
-      </Box>
-
-      <Box p={['0', '16']}
-      boxSizing='border-box'
-      py="4">
-        <Heading textAlign={'center'} size="md" mb="4" children="Users"/>
-        {/* Dought graph */}
-        <DoughnutChart />
-      </Box>
+      </GridItem>
+      {/* this item shows a piechart */}
+      <GridItem rowStart={6} colStart={2} colEnd={3} >
+        <Box p={['0', '16']}
+          boxSizing='border-box'
+          py="4">
+          <Heading textAlign={'center'} size="md" mb="4" children="Users" />
+          {/* Dought graph */}
+          <DoughnutChart />
+        </Box>
+      </GridItem>
 
     </Grid>
   );
--- src/components/Admin/Sidebar.jsx
+++ src/components/Admin/Sidebar.jsx
@@ -5,38 +5,41 @@ import {
   RiUser3Fill,
   RiAddCircleFill,
 } from 'react-icons/ri';
-import { VStack, Button } from '@chakra-ui/react';
+import { HStack, Button, Center } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
   const location = useLocation();
   return (
-    <VStack
-      spacing={'8'}
-      p="16"
-      boxShadow={'-2px 0 10px rgba(107, 70, 193, 0.5)'}
-      minH={'100vh'}
+    <HStack
+    spacing='12%'
+    direction={['column', 'row']}
+    p="2%"
     >
-      <LinkButton Icon={RiDashboardFill} text="Dashboard" url={'dashboard'} active={location.pathname==='admin/dashboard'}/>
+      <Center>
+      <LinkButton  Icon={RiDashboardFill} text="Dashboard" url={'dashboard'} active={location.pathname==='admin/dashboard'}/>
       <LinkButton Icon={RiEyeFill} text="Create Course" url={'createcourse'} active={location.pathname==="admin/createcourse"}/>
       <LinkButton Icon={RiAddCircleFill} text="Courses" url={'courses'} active={location.pathname==="admin/courses"}/>
       <LinkButton Icon={RiUser3Fill} text="User" url={'users'} active={location.pathname==="admin/users"}/>
-    </VStack>
+      </Center>
+    </HStack>
   );
 };
 export default Sidebar;
 
 function LinkButton({ url, Icon, text, active }) {
   return (
-    <Link to={`/admin/${url}`}>
-      <Button
+   
+     <Link to={`/admin/${url}`} p="2%" >
+      <Button 
         fontSize={'larger'}
         variant="ghost"
         colorScheme={active ? 'purple' : ''}
+        w="300px"
       >
         <Icon style={{ margin: '4px' }} />
-        {text}
+       {text}
       </Button>
     </Link>
   );

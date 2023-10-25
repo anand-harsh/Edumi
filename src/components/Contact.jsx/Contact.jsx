4502dfe73d094a7db68336bce7b322d19635fc6d
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2023-10-25 15:23:26 +0530

    Update last merge

--- src/components/Contact.jsx/Contact.jsx
+++ src/components/Contact.jsx/Contact.jsx
@@ -8,7 +8,7 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
 import React from 'react';
 
 const Contact = () => {
@@ -16,71 +16,81 @@ const Contact = () => {
   const [email, setEmail] = React.useState('');
   const [message, setMessage] = React.useState('');
 
-  return(
-  <Container>
-    <VStack
-    h='full'
-    justifyContent='center'
-    spacing={'16'}
-    >
-      <Heading children={'Contact Us'} />
-      <form action="" style={{ width: '100%' }}>
-        <Box my={'4'}>
-          <FormLabel htmlFor="name" children="Name" />
-          <Input
-            required
-            id="name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            placeholder="Enter your Name"
-            type="text"
-            focusBorderColor="yellow.400"
-          />
-        </Box>
+  const [viewportHeight, setViewportHeight] = useState(0);
 
-        <Box my={'4'}>
-          <FormLabel htmlFor="email" children="Email Address" />
-          <Input
-            required
-            id="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            placeholder="Abc"
-            type="email"
-            focusBorderColor="yellow.400"
-          />
-        </Box>
+  useEffect(() => {
+    const calculateViewportHeight = () => {
+      const height = window.innerHeight;
+      const offset = 134; // 20 pixels
+      setViewportHeight(height - offset);
+    };
+    calculateViewportHeight();
+    window.addEventListener('resize', calculateViewportHeight);
+    return () => {
+      window.removeEventListener('resize', calculateViewportHeight);
+    };
+  }, []);
+  return (
+    <Container>
+      <VStack justifyContent="center" spacing={'5'} h={`${viewportHeight}px`}>
+        <Heading children={'Contact Us'} />
+        <form action="" style={{ width: '100%' }}>
+          <Box my={'4'}>
+            <FormLabel htmlFor="name" children="Name" />
+            <Input
+              required
+              id="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder="Enter your Name"
+              type="text"
+              focusBorderColor="yellow.400"
+            />
+          </Box>
 
-        <Box my={'4'}>
-          <FormLabel htmlFor="message" children="Message" />
-          <Input
-            required
-            id="message"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-            placeholder="Your message"
-            focusBorderColor="yellow.400"
-          />
-        </Box>
+          <Box>
+            <FormLabel htmlFor="email" children="Email Address" />
+            <Input
+              required
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="Abc"
+              type="email"
+              focusBorderColor="yellow.400"
+            />
+          </Box>
 
-        <Button my={'4'} colorScheme="yellow" type="submit">
-          Send
-        </Button>
+          <Box my={3}>
+            <FormLabel htmlFor="message" children="Message" />
+            <Input
+              required
+              id="message"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+              placeholder="Your message"
+              focusBorderColor="yellow.400"
+            />
+          </Box>
 
-        <Box my={'4'}>
+          <Button my={3} colorScheme="yellow" type="submit">
+            Send
+          </Button>
+
+          <Box my={3}>
             Resquest For a Course?
             <Link to="/request">
-            {' '}
+              {' '}
               <Button colorScheme="yellow" variant={'link'}>
-              Click
+                Click
               </Button>{' '}
             </Link>
             here
           </Box>
-      </form>
-    </VStack>
-  </Container>
-    )
+        </form>
+      </VStack>
+    </Container>
+  );
 };
 
 export default Contact;

1ed7305cb5be36a8d2564ca7f830844fc70b622c
code-bajju <11212766@mmec.mmdu.org>
2023-10-29 13:03:35 +0530

    Initiated Notes

---
+++ src/components/Notes/Notes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Notes.css';
+
+
+
+const Notes = () => {
+  return (
+    <section className="Notes">
+      <div className="container">
+        
+      </div>
+    </section>
+  );
+};
+
+export default Notes;

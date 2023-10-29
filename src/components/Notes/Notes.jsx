332c9f8f423fb47665748953bde03f37082c3d7d
code-bajju <11212766@mmec.mmdu.org>
2023-10-29 13:17:53 +0530

    Added Notes on the left sidebar and created a new route to it. Filled the content just as Coming soon #24

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Notes.css';
-
+import {
+    Heading,
+  } from '@chakra-ui/react';
 
 
 const Notes = () => {
   return (
+    
     <section className="Notes">
+      <h1>Notes</h1>
       <div className="container">
-        <h1>Coming Soon...</h1>
-        <h1>&#128512;</h1>
+      
+        <h2>Coming Soon...&#128512;</h2>
+        <h2></h2>
       </div>
     </section>
   );

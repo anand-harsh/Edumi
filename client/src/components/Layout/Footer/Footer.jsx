b1593119d9d3455dc02c748882ced1023ac51d9e
Surajit Maity <sibu23122003@gmail.com>
2024-01-23 00:19:43 +0530

    Added new footer

--- client/src/components/Layout/Footer/Footer.jsx
+++ client/src/components/Layout/Footer/Footer.jsx
@@ -10,52 +10,118 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <Box padding={'4'} bg={'blackAlpha.800'} minH={'4vh'}>
+    <Box paddingLeft={'20'} paddingTop={'10'} bg={'#0d0b1e'} minH={'4vh'}>
       <Stack direction={['column', 'row']}>
-        <VStack alignItems={['center', 'flex-start']} width="full">
-          <Heading children="All Right Reserved" color={'white'} style={{ overflowY: "hidden"}} size={'sm'}/>
+
+        <VStack alignItems={['center', 'flex-start']} width="full" height="300px">
+        <HStack spacing={['2', '55vW']} justifyContent="center">
+        <Heading children="Let's Start with Edumi!" color={'yellow.400'} style={{ overflowY: "hidden" }} size={'lg'} />
+        <a href="mailto:harsh.anand.ggl@gmail.com">
+            <Box width={"130px"} bg={'#db3e00'} padding={'1'} borderRadius={'5px'} >
+            <Heading
+              fontFamily={'body'}
+              children="Send a message"
+              color={'white'}
+              size={'sm'}
+              style={{ overflowY: "hidden",display: "inline" }}
+              width={"200px"}
+            />
+            </Box>
+            </a>
+          </HStack>
+         
           <Heading
             fontFamily={'body'}
-            children="Contribute: https://www.github.com/anand-harsh/Edumi"
-            color={'yellow.400'}
+            children="Your journey with Edumi promises an immersive and dynamic experience, fostering an environment where education transcends traditional boundaries."
+            color={'white'}
             size={'sm'}
-            style={{ overflowY: "hidden"}}
+            style={{ overflowY: "hidden" }}
+            width={"400px"}
+            marginBottom={"20px"}
           />
+
+          <HStack spacing={['2', '5']} justifyContent="center">
+            <Heading
+              fontFamily={'body'}
+              children="Build with us"
+              color={'#db3e00'}
+              size={'md'}
+              style={{ overflowY: "hidden"}}
+              width={"150px"}
+            />
+            <a href="https://www.github.com/anand-harsh/Edumi">
+            <Box width={"130px"} bg={'white'} padding={'1'} borderRadius={'5px'}>
+            <Heading
+              fontFamily={'body'}
+              children="View on GitHub"
+              color={'black'}
+              size={'sm'}
+              style={{ overflowY: "hidden",display: "inline" }}
+              width={"200px"}
+            />
+            </Box>
+            </a>
+
+          </HStack>
+
+
+          <HStack spacing={['2', '3']} justifyContent="center" marginLeft={"30px"} marginTop={"30px"}>
+            <a href="https://www.codedev.me">
+              <TiUser
+                size="2rem"
+                color="#db3e00"
+                target={'blank'}
+                style={{ marginRight: '3px' }}
+              />
+            </a>
+            <a href="https://www.instagram.com/i.harshanand/">
+              <TiSocialInstagramCircular
+                size="2rem"
+                color="#db3e00"
+                target={'blank'}
+                style={{ marginRight: '3px' }}
+              />
+            </a>
+            <a href="https://www.github.com/anand-harsh/Edumi">
+              <DiGithub
+                size="2rem"
+                color="#db3e00"
+                target={'blank'}
+                style={{ marginRight: '3px' }}
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/anand-harsh/">
+              <TiSocialLinkedinCircular
+                size="2rem"
+                color="#db3e00"
+                target={'blank'}
+                style={{ marginRight: '3px' }}
+              />
+            </a>
+          </HStack>
+          <HStack spacing={['2', '300']} justifyContent="center" marginLeft={"35vw"}>
+          <Heading
+              fontFamily={'body'}
+              children="© 2024 Edumi. All rights reserved"
+              color={'white'}
+              size={'sm'}
+              style={{ overflowY: "hidden"}}
+              width={"300px"}
+              fontSize={'0.7rem'}
+            />
+            <Heading
+              fontFamily={'body'}
+              children="edumi"
+              color={'#db3e00'}
+              size={'lg'}
+              style={{ overflowY: "hidden"}}
+              width={"100px"}
+              // fontSize={'0.7rem'}
+              marginLeft={'1vw'}
+            />
+          </HStack>
+         
         </VStack>
-        <HStack spacing={['2', '10']} justifyContent="center">
-          <a href="https://www.codedev.me">
-            <TiUser
-              size="2.4rem"
-              color="white"
-              target={'blank'}
-              style={{ marginRight: '20px' }}
-            />
-          </a>
-          <a href="https://www.instagram.com/i.harshanand/">
-            <TiSocialInstagramCircular
-              size="2.4rem"
-              color="white"
-              target={'blank'}
-              style={{ marginRight: '20px' }}
-            />
-          </a>
-          <a href="https://www.github.com/anand-harsh/Edumi">
-            <DiGithub
-              size="2.4rem"
-              color="white"
-              target={'blank'}
-              style={{ marginRight: '20px' }}
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/anand-harsh/">
-            <TiSocialLinkedinCircular
-              size="2.4rem"
-              color="white"
-              target={'blank'}
-              style={{ marginRight: '20px' }}
-            />
-          </a>
-        </HStack>
       </Stack>
     </Box>
   );

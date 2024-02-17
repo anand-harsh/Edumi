4fc83faf41b657c576e220077e6a9647ff078dbd
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2024-02-17 19:10:31 +0530

    Update config

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://edumi-zeta.vercel.app",
     credentials: true,
   })
 );
--- client/src/config/constant.js
+++ client/src/config/constant.js
@@ -1,1 +1,1 @@
-export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+export const API_ENDPOINT = "http://localhost:4000/api/v1";

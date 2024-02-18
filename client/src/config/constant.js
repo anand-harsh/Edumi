6e4ed11068913f5338857b8d47554adc29869954
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2024-02-18 23:14:12 +0530

    Update config

--- client/src/config/constant.js
+++ client/src/config/constant.js
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = "http://localhost:4000/api/v1";
+export const API_ENDPOINT = "https://edumi-backenduri.vercel.app/api/v1";
+// export const API_ENDPOINT = "http://localhost:4000/api/v1";
